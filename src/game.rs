1147636a910359569f0ff370ff4f3bbe0a1da828
Miguel Bento <masbento@gmail.com>
2021-04-20 22:21:16 +0100

    Add Pause

--- src/game.rs
+++ src/game.rs
@@ -17,6 +17,8 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut AppBuilder) {
+        app.init_resource::<PauseMaterials>();
+
         app.add_system_set(
             SystemSet::on_enter(GameState::Game)
                 .with_system(setup_board.system())
@@ -44,7 +46,26 @@ impl Plugin for GamePlugin {
                 .with_system(handle_keyboard.system()),
         );
 
+        app.add_system_set(
+            SystemSet::on_update(GameState::Pause).with_system(handle_keyboard.system()),
+        );
+
         app.add_system_set(SystemSet::on_exit(GameState::Game).with_system(cleanup.system()));
+    }
+}
+
+struct PauseMaterials {
+    background: Handle<ColorMaterial>,
+}
+
+impl FromWorld for PauseMaterials {
+    fn from_world(world: &mut World) -> Self {
+        let mut materials = world
+            .get_resource_mut::<Assets<ColorMaterial>>()
+            .expect("world.assets#colorMaterial");
+        PauseMaterials {
+            background: materials.add(Color::rgba(1.0, 1.0, 1.0, 0.4).into()),
+        }
     }
 }
 
@@ -181,12 +202,75 @@ fn setup_walls(commands: &mut Commands, materials: &Res<MaterialsAssets>) {
 }
 
 fn handle_keyboard(
+    mut commands: Commands,
     mut keyboard_input: ResMut<Input<KeyCode>>,
     mut state: ResMut<State<GameState>>,
+    font_assets: Res<FontAssets>,
+    pause_materials: Res<PauseMaterials>,
+    text_entity: Query<(Entity, &Text)>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
         keyboard_input.reset(KeyCode::Escape);
-        state.set(GameState::Menu).expect("state: game -> menu");
+        state.replace(GameState::Menu).expect("state: game -> menu");
+    }
+
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        keyboard_input.reset(KeyCode::Space);
+        match state.current() {
+            GameState::Pause => {
+                for (entity, _text) in text_entity.iter() {
+                    commands.entity(entity).despawn_recursive();
+                }
+
+                state.pop()
+            }
+            _ => {
+                commands
+                    .spawn_bundle(NodeBundle {
+                        style: Style {
+                            size: Size::new(Val::Auto, Val::Auto),
+                            position_type: PositionType::Absolute,
+                            position: Rect {
+                                top: Val::Percent(15.0),
+                                left: Val::Percent(10.0),
+                                right: Val::Percent(10.0),
+                                ..Default::default()
+                            },
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::FlexEnd,
+                            ..Default::default()
+                        },
+                        material: pause_materials.background.clone(),
+                        ..Default::default()
+                    })
+                    .with_children(|parent| {
+                        parent.spawn_bundle(TextBundle {
+                            style: Style {
+                                align_self: AlignSelf::Center,
+                                ..Default::default()
+                            },
+                            text: Text {
+                                sections: vec![TextSection {
+                                    value: "PAUSED".to_string(),
+                                    style: TextStyle {
+                                        font: font_assets.text_font.clone(),
+                                        font_size: 72.0,
+                                        color: Color::ORANGE,
+                                    },
+                                }],
+                                alignment: TextAlignment {
+                                    horizontal: HorizontalAlign::Center,
+                                    vertical: VerticalAlign::Center,
+                                },
+                            },
+                            ..Default::default()
+                        });
+                    })
+                    .insert(Disposable);
+                state.push(GameState::Pause)
+            }
+        }
+        .expect("state: pause");
     }
 }
 
